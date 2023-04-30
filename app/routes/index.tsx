b4e176483fe6b1ae4096598dc23e8cb00faaf1cc
Jake Ruesink <jaruesink@gmail.com>
2023-04-30 22:53:58 +0000

    fix postgres on start

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,9 +1,23 @@
 import { DeferredComponent } from 'app/components/DeferredComponent';
+import { PrismaClient } from '@prisma/client';
+import { json } from '@remix-run/node';
+import { useLoaderData } from '@remix-run/react';
+
+export async function loader() {
+  const prisma = new PrismaClient();
+  const tests = await prisma.test.findMany();
+  return json({ tests });
+}
 
 export default function Index() {
+  const { tests } = useLoaderData();
+
   return (
     <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
       <h1>Welcome to Remix</h1>
+
+      <p>{JSON.stringify(tests)}</p>
+
       <ul>
         <li>
           <a
